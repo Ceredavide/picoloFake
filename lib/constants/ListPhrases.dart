36ba5cbd7b7db0476ef706da67e0286fdf109e3b
Davide <ceredavide@live.it>
2020-06-14 00:36:04 +0200

    aggiunto esempi frasi

--- lib/constants/ListPhrases.dart
+++ lib/constants/ListPhrases.dart
@@ -1,1 +1,9 @@
-const listPhrases = ["Bella Mate", "Come va"];
+const listPhrases = [
+  "Bella Mate",
+  "Come va",
+  "Flutter ci sta",
+  "React-Native è meglio però",
+  "6x8",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+];
